Fixes off-by-one bug in iha1Tests.py

Home Assignments/IHA1/utils/tests/iha1Tests.py
@@ -441,11 +441,12 @@ def test_neuralnetwork(Class):
     
     # layers dims and order tests
     assert nn.input_n == 3, "`input_n` has the wrong dimensions. Expected: {0}. Actual: {1}".format(3, nn.input_n)
+    assert dims == [2,3], "Constructor should not modify its parameters"
     _dims = [3] + dims
-    for i in range(len(nn.layers)):
-        assert nn.layers[i].w.shape == (_dims[i], _dims[i+1]), "Hidden layer {0} has the wrong weight matrix shape in a neural network with the hidden dimensions {1}. Expected: {2}. Actual: {3}".format(i+1, dims, (_dims[i-1], _dims[i]), nn.layers[i].w.shape) 
-        assert nn.layers[i].b.shape == (1,_dims[i+1]), "Hidden layer {0} has the wrong bias shape in a neural network with the hidden dimensions {1}. Expected: {2}. Actual: {3}".format(i+1, dims, (_dims[i+1], 1), nn.layers[i].b.shape) 
     
+    for i in range(len(nn.layers)-1):
+        assert nn.layers[i].w.shape == (_dims[i], _dims[i+1]), "Hidden layer {0} has the wrong weight matrix shape in a neural network with the hidden dimensions {1}. Expected: {2}. Actual: {3}".format(i+1, dims, (_dims[i], _dims[i+1]), nn.layers[i].w.shape)
+        assert nn.layers[i].b.shape == (1,_dims[i+1]), "Hidden layer {0} has the wrong bias shape in a neural network with the hidden dimensions {1}. Expected: {2}. Actual: {3}".format(i+1, dims, (_dims[i+1], 1), nn.layers[i].b.shape)
     # forward prop tests
     assert y_pred.shape == (2,3), "The shape of the output of forward_prop for the neural network is not right. Expected: (2,3). Actual: {0}".format(y_pred.shape)
     assert np.allclose(y_pred, test_case['y_pred'], rtol=1e-07, atol=1e-06), "Forward prop with relu returned the right dimensions, but the wrong output. \nExpected: {0}\nActual: \n{1}".format(test_case['y_pred'], y_pred)