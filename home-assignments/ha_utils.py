95eef07b0c03ca48d0327552f3d4de6afe5ecb4e
Lucas Brynte <brynte@chalmers.se>
2019-10-02 23:22:18 +0200

    Ridiculous update to ha_utils.py, just to comply with the fact that assignment filenames might not be the same as the directory name... Triggered by us splitting HA2 in two parts.

--- home-assignments/ha_utils.py
+++ home-assignments/ha_utils.py
@@ -2,6 +2,7 @@ import os
 import json
 import urllib
 import ssl
+import numpy as np
 
 def parse_cell_ids(nb_json_data):
     cell_ids = []
@@ -14,23 +15,48 @@ def check_notebook_uptodate_and_not_corrupted(nb_dirname, nb_fname):
     assignment_name = os.path.basename(nb_dirname)
     commit = 'master' # Should be master (latest version)
     # commit = '3d1588a79b1bd6361f6b12da9e6be022adf0f683' # For debug
-    url = 'http://raw.githubusercontent.com/JulianoLagana/deep-machine-learning/{commit}/home-assignments/{assignment_name}/{assignment_name}.ipynb'.format(assignment_name=assignment_name, commit=commit)
-    try:
-        # Create dummy SSL context object, to avoid issues. See https://stackoverflow.com/questions/27835619/urllib-and-ssl-certificate-verify-failed-error
-        ref_nb_file = urllib.request.urlopen(url, context=ssl.SSLContext())
-    except urllib.error.URLError:
-        print('[WARNING] Could not fetch reference notebook from GitHub repo. Are you offline?')
-        print('[WARNING] Could not verify that current notebook is up-to-date and not corrupted')
-        return
-    ref_nb_data = json.load(ref_nb_file)
+
+    # Fetch all of these files from server. If the cell IDs of any of them matches the cell IDs of this notebook, check should succeed.
+    nb_fname_candidates = ['HA2-Part1.ipynb', 'HA2-Part2.ipynb'] if os.path.basename(nb_dirname) == 'HA2' else [assignment_name+'.ipynb']
+
+    nbr_candidate_urls = len(nb_fname_candidates)
+    url_candidates = []
+    for expected_nb_fname in nb_fname_candidates:
+        url_candidates.append('http://raw.githubusercontent.com/JulianoLagana/deep-machine-learning/{commit}/home-assignments/{assignment_name}/{expected_nb_fname}'.format(
+            assignment_name=assignment_name,
+            expected_nb_fname=expected_nb_fname,
+            commit=commit,
+        ))
+    ref_cell_id_candidates = []
+    for url in url_candidates:
+        try:
+            # Create dummy SSL context object, to avoid issues. See https://stackoverflow.com/questions/27835619/urllib-and-ssl-certificate-verify-failed-error
+            ref_nb_file = urllib.request.urlopen(url, context=ssl.SSLContext())
+        except urllib.error.URLError:
+            print('Failed to fetch the URL: {url}'.format(url=url))
+            print('[WARNING] Could not fetch reference notebook from GitHub repo. Are you offline?')
+            print('[WARNING] Could not verify that current notebook is up-to-date and not corrupted')
+            return
+        ref_nb_data = json.load(ref_nb_file)
+        ref_cell_ids = parse_cell_ids(ref_nb_data)
+        ref_cell_ids = set(ref_cell_ids)
+        ref_cell_id_candidates.append(ref_cell_ids)
+
     with open(os.path.join(nb_dirname, nb_fname), 'r') as f:
         curr_nb_data = json.load(f)
-    ref_cell_ids = parse_cell_ids(ref_nb_data)
     curr_cell_ids = parse_cell_ids(curr_nb_data)
     assert len(curr_cell_ids) == len(set(curr_cell_ids)), \
         '[ERROR] Notebook appears to be corrupt - detected multiple cells with same cell ID. Did you copy/paste any cells?'
-    ref_cell_ids = set(ref_cell_ids)
     curr_cell_ids = set(curr_cell_ids)
+
+    # Determine URL, by seeing which matches most cell IDs
+    nbr_matching_cells = [len(curr_cell_ids & ref_cell_ids) for ref_cell_ids in ref_cell_id_candidates]
+    idx_matched_candidate = np.argmax(nbr_matching_cells)
+    url = url_candidates[idx_matched_candidate]
+    ref_cell_ids = ref_cell_id_candidates[idx_matched_candidate]
+
+    print('Matching current notebook against the following URL:\n{url}'.format(url=url))
+
     assert len(curr_cell_ids) > 0, \
         '[ERROR] Notebook appears to be corrupt - no cell IDs found. Did you perhaps run it on Google Colab?'
     if len(ref_cell_ids - curr_cell_ids) > 0:
